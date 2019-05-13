608d3b0b8fe50cfcde0d4b7248658543371b7342
Max Albrecht <max.albrecht@zhdk.ch>
2019-05-13 19:44:03 +0200

    $ rails g graphql:object MediaEntry id:String

---
+++ app/graphql/types/media_entry_type.rb
@@ -0,0 +1,5 @@
+module Types
+  class MediaEntryType < Types::BaseObject
+    field :id, String, null: true
+  end
+end
---
+++ app/models/media_entry.rb
@@ -0,0 +1,14 @@
+class MediaEntry < ApplicationRecord
+  # has_many :collection_media_entry_arcs,
+  #          class_name: 'Arcs::CollectionMediaEntryArc'
+  # has_many :parent_collections,
+  #          through: :collection_media_entry_arcs,
+  #          source: :collection
+  #
+  # has_one :media_file
+
+  scope :public_visible, -> { where(get_metadata_and_previews: true) }
+  scope :published, -> { where(is_published: true) }
+  default_scope { published.public_visible }
+
+end
