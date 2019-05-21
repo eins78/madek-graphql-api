813ee58e279004598b7b520669f68a568dcf0348
Marek Lotkowski <marek.lotkowski@gmail.com>
2019-05-21 21:16:13 +0200

    Moved #title method to MediaEntry model

--- app/graphql/types/media_entry_type.rb
+++ app/graphql/types/media_entry_type.rb
@@ -3,10 +3,5 @@ module Types
     field :id, String, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: true
     field :title, String, null: true
-
-    def title
-      MetaDatum.where(meta_key_id: 'madek_core:title',
-                      media_entry_id: self.object.id).take.string
-    end
   end
 end
--- app/models/media_entry.rb
+++ app/models/media_entry.rb
@@ -15,4 +15,8 @@ class MediaEntry < ApplicationRecord
   scope :public_visible, -> { where(get_metadata_and_previews: true) }
   scope :published, -> { where(is_published: true) }
   default_scope { published.public_visible }
+
+  def title
+    meta_data.where(meta_key_id: 'madek_core:title').take.string
+  end
 end
