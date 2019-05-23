e89b37577bd802ac30d26d3d76bbce2902b025f5
Marek Lotkowski <marek.lotkowski@gmail.com>
2019-05-23 15:26:09 +0200

    Reorganized specs in more reasonable way to reflect code structure better

--- spec/graphql/types/media_entry_type_spec.rb
+++ spec/graphql/types/media_entry_type_spec.rb
@@ -1,24 +1,49 @@
 require 'rails_helper'
 
 describe Types::MediaEntryType do
-  let(:media_entry) { FactoryGirl.create(:media_entry_with_title) }
-  let(:response_data) { response_to_h[:data][:mediaEntry] }
 
-  it 'returns id' do
-    media_entry
-    expect(response_data[:id]).to eq(media_entry.id)
+  context 'type definition' do
+
+    subject { Types::MediaEntryType }
+
+    it 'defines a field id of String type' do
+      expect(subject).to have_field(:id).that_returns(type('String'))
+    end
+
+    it 'defines a field created_at of String type' do
+      expect(subject).to have_field(:createdAt).that_returns(type('ISO8601DateTime'))
+    end
+
+    it 'defines a field title of String type' do
+      expect(subject).to have_field(:id).that_returns(type('String'))
+    end
   end
 
-  it 'returns createdAt in ISO 8601 standard' do
-    standarized_created_at = media_entry.created_at.iso8601
-    expect(response_data[:createdAt]).to eq(standarized_created_at)
+
+  context 'response' do
+
+    let(:media_entry) { FactoryGirl.create(:media_entry_with_title) }
+    let(:response_data) { response_to_h[:data][:mediaEntry] }
+
+    it 'returns id' do
+      media_entry
+      expect(response_data[:id]).to eq(media_entry.id)
+    end
+
+    it 'returns createdAt in ISO 8601 standard' do
+      standarized_created_at = media_entry.created_at.iso8601
+      expect(response_data[:createdAt]).to eq(standarized_created_at)
+    end
+
+    it 'returns title' do
+      expect(response_data[:title]).to eq(media_entry.title)
+    end
   end
 
-  it 'returns title' do
-    expect(response_data[:title]).to eq(media_entry.title)
+
+  def type(type)
+    "GraphQL::Types::#{type}"
   end
-
-
 
   def response_to_h
     query = get_media_entry_by_id(media_entry.id)
--- spec/graphql/types/query_type_spec.rb
+++ spec/graphql/types/query_type_spec.rb
@@ -1,32 +1,15 @@
 describe Types::QueryType do
 
-  context 'MediaEntryType' do
+  describe 'MediaEntryType fields' do
 
-    subject { Types::MediaEntryType }
+    describe Types::QueryType.fields['mediaEntry'] do
 
-    it 'requires and "id" argument of ID type' do
-      id_argument = Types::QueryType.fields['mediaEntry'].arguments['id']
+      it 'requires and "id" argument of ID type' do
+        id_argument = subject.arguments['id']
 
-      expect(id_argument.type.class).to be(GraphQL::Schema::NonNull)
-      expect(id_argument.type.of_type).to be(GraphQL::Types::ID)
-    end
-
-    it 'defines a field id of String type' do
-      expect(subject).to have_field(:id).that_returns(type('String'))
-    end
-
-    it 'defines a field created_at of String type' do
-      expect(subject).to have_field(:createdAt).that_returns(type('ISO8601DateTime'))
-    end
-
-    it 'defines a field title of String type' do
-      expect(subject).to have_field(:id).that_returns(type('String'))
-    end
-
-
-
-    def type(type)
-      "GraphQL::Types::#{type}"
+        expect(id_argument.type.class).to be(GraphQL::Schema::NonNull)
+        expect(id_argument.type.of_type).to be(GraphQL::Types::ID)
+      end
     end
   end
 end
