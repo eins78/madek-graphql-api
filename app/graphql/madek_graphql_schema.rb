15734c72590f36172675b4a79107cd502f4cc997
Marek Lotkowski <marek.lotkowski@gmail.com>
2019-05-27 18:04:18 +0200

    Added OrderByEnum to the schema

--- app/graphql/madek_graphql_schema.rb
+++ app/graphql/madek_graphql_schema.rb
@@ -4,5 +4,16 @@ class MadekGraphqlSchema < GraphQL::Schema
   # use GraphQL::Analysis::AST
 
   # mutation(Types::MutationType)
+  OrderByEnum = GraphQL::EnumType.define do
+    name 'OrderByEnum'
+    description 'Paremeter for ordering resources'
+
+    value 'CREATED_AT_DESC', value: 'created_at DESC'
+    value 'CREATED_AT_ASC', value: 'created_at ASC'
+  end
+
   query(Types::QueryType)
+
+
+
 end
