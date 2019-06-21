0ffdd3e8c54da5f8bb2ee9c5167e55b4f0c90263
Max Albrecht <max.albrecht@zhdk.ch>
2019-06-21 10:30:53 +0200

    load fewer frameworks from rails

--- config/application.rb
+++ config/application.rb
@@ -5,13 +5,13 @@ require_relative 'boot'
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
-require "active_job/railtie"
+# require "active_job/railtie"
 require "active_record/railtie"
-require "active_storage/engine"
+# require "active_storage/engine"
 require "action_controller/railtie"
-require "action_mailer/railtie"
+# require "action_mailer/railtie"
 require "action_view/railtie"
-require "action_cable/engine"
+# require "action_cable/engine"
 require "sprockets/railtie" if Rails.env.development?
 require "rails/test_unit/railtie"
 
@@ -45,4 +45,3 @@ module MadekGraphqlApi
       Rails.root.join('datalayer', 'initializers', '030_factory_girl.rb')
   end
 end
-
